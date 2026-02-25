8d6ed39392e77012ee77e7f7f47be40aa5ab2a60
Alexey Schastlivtsev <matuation@gmail.com>
2026-02-26 00:53:47 +0300

    Задание #2. Сравнение объектов
    Вы познакомились с основными универсальными методами

--- src/Main.java
+++ src/Main.java
@@ -14,34 +14,40 @@ public class Main {
             boolean fileExist = file.exists();
             boolean isDirectory = file.isDirectory();
             int totalLines = 0;
-            int longestLine = 0;
-            int shortestLine = 1024;
+            int yandexBot = 0;
+            int googleBot = 0;
             if (fileExist && !isDirectory) {
                 System.out.println("Путь указан верно");
                 goodEntryCounter++;
                 System.out.println("Это файл номер " + goodEntryCounter);
                 try {
                     FileReader fileReader = new FileReader(path);
-                    BufferedReader reader =
-                            new BufferedReader(fileReader);
+                    BufferedReader reader = new BufferedReader(fileReader);
                     String line;
                     while ((line = reader.readLine()) != null) {
                         int length = line.length();
                         totalLines++;
-                        if (length > longestLine) {
-                            longestLine = length;
-                            shortestLine = longestLine;
-                        }
-                        if (length < shortestLine) {
-                            shortestLine = length;
+                        String result = "";
+                        int start = line.indexOf("(") + 1;
+                        int end = line.lastIndexOf(")");
+                        if (start > 0 && end > start) {
+                            result = line.substring(start, end);
+                            if (result.contains("Googlebot")) {
+                                googleBot++;
+
+                            } else if (result.contains("YandexBot")) {
+                                yandexBot++;
+
+                            }
                         }
                         if (length > 1024) {
                             throw new exceededLineLengthException();
                         }
                     }
                     System.out.println("Файл содержит " + totalLines + " строк");
-                    System.out.println("Символов в самой длинной строке: " + longestLine);
-                    System.out.println("Символов в самой короткой строке: " + shortestLine);
+                    System.out.println("Запросов от YandexBot: " + yandexBot);
+                    System.out.println("Запросов от Googlebot: " + googleBot);
+
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
